f82c8ec52e49565fbb45b394803b0012ebb86594
Tobias Jahodinsky <wi21b009@technikum-wien.at>
2022-06-22 15:28:22 +0200

    Initial commit

---
+++ src/main/java/at/jahodinsky/slm_test/SlmTestApplication.java
@@ -0,0 +1,13 @@
+package at.jahodinsky.slm_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/jahodinsky/slm_test/SlmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jahodinsky.slm_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
